96c1eb4c415640300c0e2f0f8d2196b709e3e3c3
Robert Diaz Bejar <robertdibejar@gmail.com>
2022-03-16 01:04:35 -0300

    testing deploy

--- golf-scraper.js
+++ golf-scraper.js
@@ -125,6 +125,10 @@ const reservetionCheck = (tee) => {
     const { dateIsOk, link } = checkDate(dataBox);
     if (!dateIsOk) {
       console.log("Not today");
+      const currentDay = new Date().toLocaleString("es-AR", {timeZone: "America/Argentina/Buenos_Aires"}).replaceAll("/","-").replace(/\s/g, '');
+      await page.screenshot({ 
+        path: `${currentDay}.png`
+      });
       await browser.close();
       return;
     }
